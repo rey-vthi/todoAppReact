d641b1312753a9230bc7bef41883805ce1e3b6ac
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-01 11:12:27 +0530

    made object lookup for styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-const getIndicators = function(status) {
+const Todo = function(props) {
   const style = {
     done: {color: 'todo-box done-todo', textIndicator: 'strike-through'},
     working: {color: 'todo-box processing-todo', textIndicator: 'text'},
     undone: {color: 'todo-box pending-todo', textIndicator: 'text'}
   };
-  return style[status];
-};
-
-const Todo = function(props) {
-  const {color, textIndicator} = getIndicators(props.status);
+  const {color, textIndicator} = style[props.status];
   return (
     <div className="todo" onClick={() => props.toggleStatus(props.id)}>
       <div className={color}></div>
