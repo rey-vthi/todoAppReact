4918becce6f45f18b2b55e42d0f3c6d51db8a3de
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-02 13:49:09 +0530

    refactored Todo functional component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const Todo = function(props) {
+  const {status, id, toggleStatus, removeTodo, todo} = props;
   return (
     <div className="todo">
-      <div
-        className={`task ${props.status}`}
-        onClick={() => props.toggleStatus(props.id)}
-      >
-        {props.todo}
+      <div className={`task ${status}`} onClick={() => toggleStatus(id)}>
+        {todo}
       </div>
-      <div className="remove" onClick={() => props.removeTodo(props.id)}>
+      <div className="remove" onClick={() => removeTodo(id)}>
         X
       </div>
     </div>
