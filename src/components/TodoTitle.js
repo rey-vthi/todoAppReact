ac1650024aa1d9e753b1562b1fb1c8fa945272cd
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-05 14:01:08 +0530

    made todoTitle use hooks

--- src/components/TodoTitle.js
+++ src/components/TodoTitle.js
@@ -1,43 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import InputBox from './InputBox';
 import '../App.css';
 import Delete from './Delete';
 
-class TodoTitle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {isEditable: false};
-    this.updateTitle = this.updateTitle.bind(this);
-    this.toggleEditStatus = this.toggleEditStatus.bind(this);
-  }
+const TodoTitle = function(props) {
+  const [isEditable, setEditable] = useState(false);
 
-  updateTitle(title) {
-    this.props.updateTitle(title);
-    this.toggleEditStatus();
-  }
+  const toggleEditStatus = function() {
+    setEditable(s => !s);
+  };
 
-  toggleEditStatus() {
-    this.setState(state => ({isEditable: !state.isEditable}));
-  }
+  const updateNewTitle = function(title) {
+    props.updateTitle(title);
+    toggleEditStatus();
+  };
 
-  render() {
-    const editableText = (
-      <InputBox
-        className="todo-title"
-        value={this.props.title}
-        onEnter={this.updateTitle}
-      />
-    );
-    const text = (
-      <div className="todo">
-        <h2 className="todo-title" onClick={this.toggleEditStatus}>
-          {this.props.title}
-        </h2>
-        <Delete remove={this.props.removeAllTodo} />
-      </div>
-    );
-    return <div>{this.state.isEditable ? editableText : text}</div>;
-  }
-}
+  const editableText = (
+    <InputBox
+      className="todo-title"
+      value={props.title}
+      onEnter={updateNewTitle}
+    />
+  );
+
+  const text = (
+    <div className="todo">
+      <h2 className="todo-title" onClick={toggleEditStatus}>
+        {props.title}
+      </h2>
+      <Delete remove={props.removeAllTodo} />
+    </div>
+  );
+
+  return <div>{isEditable ? editableText : text}</div>;
+};
 
 export default TodoTitle;
