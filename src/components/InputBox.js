f90b948acf28645be687815933e739ecc21d05cf
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-08 18:03:52 +0530

    cleaned up input box commented lines

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,36 +1,5 @@
 import React, {useState} from 'react';
 
-// class InputBox extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {text: props.value};
-//     this.handleChange = this.handleChange.bind(this);
-//     this.addTodo = this.addTodo.bind(this);
-//   }
-
-//   handleChange(event) {
-//     const text = event.target.value;
-//     this.setState({text});
-//   }
-
-//   addTodo(event) {
-//     if (event.keyCode === 13 && this.state.text !== '') {
-//       this.props.onEnter(this.state.text);
-//       return this.setState({text: ''});
-//     }
-//   }
-//   render() {
-//     return (
-//       <input
-//         className="todo-feeder"
-//         onChange={this.handleChange}
-//         onKeyDown={this.addTodo}
-//         value={this.state.text}
-//       ></input>
-//     );
-//   }
-// }
-
 const InputBox = function(props) {
   const [text, setText] = useState(props.value);
 
